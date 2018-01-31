c29abc8f8333413716d756d104bb9cae05c03735
agrawal-prateek <prateekagrawal89760@gmail.com>
2018-02-01 01:21:07 +0530

    Commited from Pycharm

--- src/dependencies.sh
+++ src/dependencies.sh
@@ -2,29 +2,31 @@
 
 # install google-cloud-sdk
 
-#if ! [ -x "$(command -v gcloud)" ]; then
-#  tar -xvzf src/lib/google-cloud-sdk.tar.gz
-#  google-cloud-sdk/install.sh
-#  rm -r google-cloud-sdk
+#if [ -x "$(command -v gcloud)" ]; then
+#  rm -rf /opt/google-cloud-sdk
+#  rm -f /usr/bin/gcloud
+#  tar -xvzf src/lib/google-cloud-sdk.tar.gz -C /opt
+#  /opt/google-cloud-sdk/install.sh
+#  ln -s /opt/google-cloud-sdk/bin/gcloud /usr/bin/gcloud
 #fi
 
 # install google-cloud-speech python library
-#pip install --upgrade google-cloud-speech
+#pip install --upgrade google-cloud-speech==0.30.0
 
 # install portaudio C/C++ library
 
-#tar -xvzf src/lib/portaudio.tar.gz
-#cd portaudio
+#tar -xvzf src/lib/portaudio.tar.gz -C /tmp
+#cd /tmp/portaudio
 #./configure
 #make
 #make install
-#cd ../
-#rm -r portaudio
+#cd -
+#rm -rf /tmp/portaudio
 
 # install PyAudio-0.2.11 python library
 
-#tar -xvzf src/lib/PyAudio-0.2.11.tar.gz
-#cd PyAudio-0.2.11
+#tar -xvzf src/lib/PyAudio-0.2.11.tar.gz -C /tmp
+#cd /tmp/PyAudio-0.2.11
 #python setup.py install
-#cd ../
-#rm -r PyAudio-0.2.11
+#cd -
+#rm -rf /tmp/PyAudio-0.2.11
